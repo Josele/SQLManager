7a9b99c61ac96b82165ceea9a070306bfcd947d3
Josele <jl.moragas@hotmail.es>
2015-06-29 04:56:21 +0200

    29/06/2015 4:56
    Preview of layout and some functionalities

--- SQLManagerMain.cpp
+++ SQLManagerMain.cpp
@@ -47,9 +47,15 @@ wxString wxbuildinfo(wxbuildinfoformat format)
 }
 
 //(*IdInit(SQLManagerFrame)
-const long SQLManagerFrame::ID_MENUITEM1 = wxNewId();
-const long SQLManagerFrame::idMenuQuit = wxNewId();
+const long SQLManagerFrame::ID_LISTBOX1 = wxNewId();
+const long SQLManagerFrame::ID_STATICLINE1 = wxNewId();
+const long SQLManagerFrame::ID_TEXTCTRL1 = wxNewId();
+const long SQLManagerFrame::ID_deleteitem = wxNewId();
+const long SQLManagerFrame::ID_Save = wxNewId();
+const long SQLManagerFrame::ID_Run = wxNewId();
+const long SQLManagerFrame::ID_MenuNew = wxNewId();
 const long SQLManagerFrame::idMenuLoad = wxNewId();
+const long SQLManagerFrame::idMenuQuit = wxNewId();
 const long SQLManagerFrame::idMenuAbout = wxNewId();
 const long SQLManagerFrame::ID_STATUSBAR1 = wxNewId();
 //*)
@@ -65,18 +71,37 @@ SQLManagerFrame::SQLManagerFrame(wxWindow* parent,wxWindowID id)
     wxMenuItem* MenuItem2;
     wxMenuItem* MenuItem1;
     wxMenu* Menu1;
+    wxBoxSizer* BoxSizer1;
     wxMenuBar* MenuBar1;
+    wxFlexGridSizer* FlexGridSizer1;
     wxMenu* Menu2;
 
     Create(parent, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("wxID_ANY"));
+    FlexGridSizer1 = new wxFlexGridSizer(0, 3, 0, 0);
+    ListBox1 = new wxListBox(this, ID_LISTBOX1, wxDefaultPosition, wxSize(132,224), 0, 0, 0, wxDefaultValidator, _T("ID_LISTBOX1"));
+    FlexGridSizer1->Add(ListBox1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    StaticLine1 = new wxStaticLine(this, ID_STATICLINE1, wxDefaultPosition, wxSize(1,208), wxLI_VERTICAL, _T("ID_STATICLINE1"));
+    FlexGridSizer1->Add(StaticLine1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    TextCtrl1 = new wxTextCtrl(this, ID_TEXTCTRL1, _("Text"), wxDefaultPosition, wxSize(532,228), 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
+    FlexGridSizer1->Add(TextCtrl1, 1, wxALL|wxALIGN_RIGHT|wxALIGN_TOP, 5);
+    Delete = new wxButton(this, ID_deleteitem, _("Delete"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_deleteitem"));
+    FlexGridSizer1->Add(Delete, 1, wxALL|wxALIGN_BOTTOM|wxALIGN_CENTER_HORIZONTAL, 5);
+    FlexGridSizer1->Add(0,0,1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
+    Save = new wxButton(this, ID_Save, _("Save"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_Save"));
+    BoxSizer1->Add(Save, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Run = new wxButton(this, ID_Run, _("Run"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_Run"));
+    BoxSizer1->Add(Run, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    FlexGridSizer1->Add(BoxSizer1, 1, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5);
+    SetSizer(FlexGridSizer1);
     MenuBar1 = new wxMenuBar();
     Menu1 = new wxMenu();
-    MenuItem3 = new wxMenuItem(Menu1, ID_MENUITEM1, _("New db\tAlt-N"), _("Create a new data base"), wxITEM_NORMAL);
+    MenuItem3 = new wxMenuItem(Menu1, ID_MenuNew, _("New db\tAlt-N"), _("Create a new data base"), wxITEM_NORMAL);
     Menu1->Append(MenuItem3);
-    MenuItem1 = new wxMenuItem(Menu1, idMenuQuit, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
-    Menu1->Append(MenuItem1);
     MenuItem4 = new wxMenuItem(Menu1, idMenuLoad, _("Load db\tAlt-L"), _("Load a data Base"), wxITEM_NORMAL);
     Menu1->Append(MenuItem4);
+    MenuItem1 = new wxMenuItem(Menu1, idMenuQuit, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
+    Menu1->Append(MenuItem1);
     MenuBar1->Append(Menu1, _("&File"));
     Menu2 = new wxMenu();
     MenuItem2 = new wxMenuItem(Menu2, idMenuAbout, _("About\tF1"), _("Show info about this application"), wxITEM_NORMAL);
@@ -90,12 +115,14 @@ SQLManagerFrame::SQLManagerFrame(wxWindow* parent,wxWindowID id)
     StatusBar1->SetStatusStyles(1,__wxStatusBarStyles_1);
     SetStatusBar(StatusBar1);
     FileDialog1 = new wxFileDialog(this, _("Select file"), wxEmptyString, wxEmptyString, wxFileSelectorDefaultWildcardStr, wxFD_DEFAULT_STYLE, wxDefaultPosition, wxDefaultSize, _T("wxFileDialog"));
+    FlexGridSizer1->Fit(this);
+    FlexGridSizer1->SetSizeHints(this);
 
-    Connect(ID_MENUITEM1,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnMenuNewSelected);
-    Connect(idMenuQuit,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnQuit);
+    Connect(ID_LISTBOX1,wxEVT_COMMAND_LISTBOX_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnListBox1Select);
+    Connect(ID_MenuNew,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnMenuNewSelected);
     Connect(idMenuLoad,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnMenuLoadSelected);
+    Connect(idMenuQuit,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnQuit);
     Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&SQLManagerFrame::OnAbout);
-    Connect(wxID_ANY,wxEVT_CLOSE_WINDOW,(wxObjectEventFunction)&SQLManagerFrame::OnClose);
     //*)
 
      db =0;
@@ -127,7 +154,8 @@ void SQLManagerFrame::OnClose(wxCloseEvent& event)
 {
     sqlite3_close(db);
     FreeLibrary(histDLL);
-    wxWindow::Destroy();
+      //  Close();
+   wxWindow::Destroy();
 }
 
 
@@ -161,3 +189,7 @@ FreeLibrary(histDLL);
 
 
 
+
+void SQLManagerFrame::OnListBox1Select(wxCommandEvent& event)
+{
+}
--- SQLManagerMain.h
+++ SQLManagerMain.h
@@ -11,8 +11,13 @@
 #define SQLMANAGERMAIN_H
 
 //(*Headers(SQLManagerFrame)
+#include <wx/sizer.h>
 #include <wx/menu.h>
+#include <wx/textctrl.h>
+#include <wx/listbox.h>
+#include <wx/statline.h>
 #include <wx/filedlg.h>
+#include <wx/button.h>
 #include <wx/frame.h>
 #include <wx/statusbr.h>
 //*)
@@ -35,14 +40,20 @@ class SQLManagerFrame: public wxFrame
         void OnAbout(wxCommandEvent& event);
         void OnClose(wxCloseEvent& event);
         void OnMenuNewSelected(wxCommandEvent& event);
-        void OnClose1(wxCloseEvent& event);
         void OnMenuLoadSelected(wxCommandEvent& event);
+        void OnListBox1Select(wxCommandEvent& event);
         //*)
 
         //(*Identifiers(SQLManagerFrame)
-        static const long ID_MENUITEM1;
-        static const long idMenuQuit;
+        static const long ID_LISTBOX1;
+        static const long ID_STATICLINE1;
+        static const long ID_TEXTCTRL1;
+        static const long ID_deleteitem;
+        static const long ID_Save;
+        static const long ID_Run;
+        static const long ID_MenuNew;
         static const long idMenuLoad;
+        static const long idMenuQuit;
         static const long idMenuAbout;
         static const long ID_STATUSBAR1;
         //*)
@@ -50,8 +61,14 @@ class SQLManagerFrame: public wxFrame
         //(*Declarations(SQLManagerFrame)
         wxMenuItem* MenuItem4;
         wxFileDialog* FileDialog1;
+        wxButton* Save;
+        wxButton* Delete;
         wxMenuItem* MenuItem3;
         wxStatusBar* StatusBar1;
+        wxStaticLine* StaticLine1;
+        wxTextCtrl* TextCtrl1;
+        wxButton* Run;
+        wxListBox* ListBox1;
         //*)
 
         DECLARE_EVENT_TABLE()
